5613b0822756c127b4357bbedc15ac66faefb7c3
shabnamshahreen <sifat@nascenia.com>
2013-07-19 17:20:59 +0600

    relation updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :chocolatiers
+  has_many :chocolatiers, :dependent => :destroy
+  has_many :ratings , :dependent => :destroy
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,7 +8,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :uid, :provider ,:admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :uid, :provider ,:admin, :photo , :user_name ,:fb_photo
+  mount_uploader :photo , ImageUploader
   # attr_accessible :title, :body
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
@@ -22,6 +24,10 @@ class User < ActiveRecord::Base
                            uid:auth.uid,
                            email:auth.info.email,
                            password:Devise.friendly_token[0,20],
+                           user_name:auth.info.name,
+                           fb_photo:auth.info.image,
+                           admin:0
+
         )
       end
 
