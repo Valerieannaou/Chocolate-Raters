a57d7c2f406e6bd255a1fb3ce0e8943be0baedce
shabnamshahreen <sifat@nascenia.com>
2013-07-21 17:05:42 +0600

    homepage completed

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def home
     @reviews = Rating.all(:order => 'created_at desc', :limit => 5)
+    @chocolates = Chocolate.all.sample(2)
   end
 end
