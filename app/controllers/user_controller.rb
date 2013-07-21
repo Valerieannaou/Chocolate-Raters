1998c09a919c5cb0a005248cbfb9b84d590d621f
shabnamshahreen <sifat@nascenia.com>
2013-07-21 17:06:10 +0600

    user deletion added

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+
+  authorize_resource :class => false
+  def index
+    @users= User.all
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_index_path }
+      format.json { head :no_content }
+    end
+  end
+
+end
