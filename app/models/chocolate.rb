b7d0a534343f71860da3b348616953dcb833fbe5
shabnamshahreen <sifat@nascenia.com>
2013-07-22 17:27:55 +0600

    scope search commented out

--- app/models/chocolate.rb
+++ app/models/chocolate.rb
@@ -1,11 +1,9 @@
 class Chocolate < ActiveRecord::Base
-  attr_accessible :description, :name, :photos_attributes , :image ,:type_of_chocolate, :cocao, :chocolatier_id
+  attr_accessible :description, :name, :photos_attributes , :image ,:type_of_chocolate, :cocoa, :chocolatier_id
   belongs_to :chocolatier
   has_many :photos, :dependent => :destroy
   has_many :ratings, :dependent => :destroy
   accepts_nested_attributes_for :photos, :allow_destroy => true ,:reject_if => :all_blank#, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
   #mount_uploader :image , ImageUploader
-  scope :by_chocolate_name , lambda{ |name| where('name LIKE ?', "%#{name}%" ) unless name.nil? }
-
-
+  #scope :by_chocolate_name , lambda{ |name| where('name LIKE ?', "%#{name}%" ) unless name.nil? }
 end
