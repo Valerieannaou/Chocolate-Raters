a342cd5b5c8797a40a01b0396ca751c242515ad6
shabnamshahreen <sifat@nascenia.com>
2013-07-22 17:25:57 +0600

    sphinx search added

---
+++ app/indices/chocolate_index.rb
@@ -0,0 +1,6 @@
+ThinkingSphinx::Index.define :chocolate, :with => :active_record do
+  indexes :name , sortable: true
+  indexes chocolatier(:name) , :as => :chocolatier_name
+  set_property :enable_star => 1
+  set_property :min_infix_len => 2
+end
