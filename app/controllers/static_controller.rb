701f0d86db7f6b24198923dc3f0609d1f00bd455
shabnamshahreen <sifat@nascenia.com>
2013-07-23 17:27:26 +0600

    updated

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
+  layout "home"
   def home
-    @reviews = Rating.all(:order => 'created_at desc', :limit => 5)
+    @reviews = Rating.all(:order => 'created_at desc', :limit => 6)
     @chocolates = Chocolate.all.sample(2)
   end
 end
