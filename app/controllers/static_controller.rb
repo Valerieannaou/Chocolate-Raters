0a1221e9a7b72deec19f908ab8dfee02ef10d61b
shabnamshahreen <sifat@nascenia.com>
2013-07-24 17:50:34 +0600

    data fetching modified

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,9 @@
 class StaticController < ApplicationController
   layout "home"
   def home
+    #@reviews = nil
     @reviews = Rating.all(:order => 'created_at desc', :limit => 6)
     @chocolates = Chocolate.all.sample(2)
+    #@chocolates = nil
   end
 end
