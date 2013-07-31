8b909e59dd92d48de236d47dfe204e72c190cd98
shabnamshahreen <sifat@nascenia.com>
2013-07-31 16:18:22 +0600

    validation updated for chocolatier

--- app/models/chocolatier.rb
+++ app/models/chocolatier.rb
@@ -7,8 +7,10 @@ class Chocolatier < ActiveRecord::Base
   #has_many :business_hours , :dependent => :destroy
   #accepts_nested_attributes_for :business_hours, :allow_destroy => true ,:reject_if => :all_blank
   #scope :by_chocolatier_name , lambda{ |name| where('name LIKE ?', "%#{name}%" ) unless name.nil? }
-  validates :name, :email ,:country , presence: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  #after_save ThinkingSphinx::RealTime.callback_for(:chocolatier)
+  validates :name, :chocolatier_url , presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i , :allow_blank => true
   validates_format_of :chocolatier_url, :with => URI::regexp
+  validates_numericality_of :phone, :allow_blank => true
 
 end
