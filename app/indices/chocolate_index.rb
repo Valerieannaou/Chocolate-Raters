bc06e9cc2ad12b03255b71a2af9f96dee68e78f2
shabnamshahreen <sifat@nascenia.com>
2013-07-31 16:16:45 +0600

    indexing slightly chaned

--- app/indices/chocolate_index.rb
+++ app/indices/chocolate_index.rb
@@ -1,6 +1,7 @@
 ThinkingSphinx::Index.define :chocolate, :with => :active_record do
-  indexes :name , sortable: true
-  indexes chocolatier(:name) , :as => :chocolatier_name
+  indexes name , sortable: true
+  indexes chocolatier.name , :as => :chocolatier_name
   set_property :enable_star => 1
   set_property :min_infix_len => 2
+  has created_at
 end
