15064cfc3cc73f10589ebb69bcb0682694585e77
shabnamshahreen <sifat@nascenia.com>
2013-08-06 12:06:37 +0600

    photo field added in chocolatier

--- app/models/chocolatier.rb
+++ app/models/chocolatier.rb
@@ -1,9 +1,10 @@
 class Chocolatier < ActiveRecord::Base
-  attr_accessible :address, :chocolatier_type, :chocolatier_url, :city, :country, :email, :name, :phone, :state, :zip_code
+  attr_accessible :address, :chocolatier_type, :chocolatier_url, :city, :country, :email, :name, :phone, :state, :zip_code ,:photo
   belongs_to :user
   has_many :chocolates , :dependent => :destroy
   scope :by_active_status ,where(status: 1)
   scope :by_pending_status ,where(status: 0)
+  mount_uploader :photo , ImageUploader
   #has_many :business_hours , :dependent => :destroy
   #accepts_nested_attributes_for :business_hours, :allow_destroy => true ,:reject_if => :all_blank
   #scope :by_chocolatier_name , lambda{ |name| where('name LIKE ?', "%#{name}%" ) unless name.nil? }
